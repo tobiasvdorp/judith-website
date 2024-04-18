18f50544f9487b61de9f783a94d0807ece08af6e
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-04-18 12:53:15 +0200

    Update page.tsx to render Builder content dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import ModeToggle from "@/components/ThemeSwitcher";
+import React from "react";
+import { builder } from "@builder.io/sdk";
+import Head from "next/head";
+import { RenderBuilderContent } from "@/components/builder";
 
-export default function Home() {
+// Replace with your Public API Key
+builder.init("87f7e6ddda884039ad862d083035a471");
+
+interface PageProps {
+  params: {
+    page: string[];
+  };
+}
+
+export default async function Page(props: PageProps) {
+  const homepageContent = await builder
+    .get("homepage", {
+      prerender: false,
+    })
+    .toPromise();
+
   return (
-    <div className="">
-      Hoi <ModeToggle />
-    </div>
+    <>
+      <Head>
+        <title>{homepageContent?.data.title}</title>
+      </Head>
+      {/* Render the Builder page */}
+      <RenderBuilderContent model="homepage" content={homepageContent} />
+    </>
   );
 }
