8520080b5193d6b47bd19cec52ef0fea59c1b4b6
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-04-22 12:23:32 +0200

    Homepage added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { builder } from "@builder.io/sdk";
-import Head from "next/head";
 import { RenderBuilderContent } from "@/components/builder";
+import { builder } from "@builder.io/sdk";
 
-// Replace with your Public API Key
-builder.init("87f7e6ddda884039ad862d083035a471");
+// Builder Public API Key set in .env file
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 interface PageProps {
   params: {
@@ -13,19 +11,23 @@ interface PageProps {
 }
 
 export default async function Page(props: PageProps) {
-  const homepageContent = await builder
-    .get("homepage", {
-      prerender: false,
+  const builderModelName = "page";
+
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get(builderModelName, {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+      },
     })
+    // Convert the result to a promise
     .toPromise();
 
   return (
     <>
-      <Head>
-        <title>{homepageContent?.data.title}</title>
-      </Head>
       {/* Render the Builder page */}
-      <RenderBuilderContent model="homepage" content={homepageContent} />
+      <RenderBuilderContent content={content} model={builderModelName} />
     </>
   );
 }
