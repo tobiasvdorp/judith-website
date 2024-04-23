116239e377be5f622f635731c9d0af5057bae17a
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-04-23 10:27:57 +0200

    Move sectioncard

--- components/molecules/what-i-do/SectionCard.tsx
+++ components/molecules/what-i-do/SectionCard.tsx
@@ -1,39 +1,37 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import Title from "@/components/Standard/Title";
 import Text from "@/components/Standard/Text";
-import Button from "../Standard/Button";
-import CustomLink from "../Standard/CustomLink";
+import CustomLink from "../../Standard/CustomLink";
 
-type SectionCard = {
+export type SectionCardProps = {
   imageSrc: string;
   title: string;
   description: string;
   buttonRef: string;
 };
 
-export default function SectionCard({
-  imageSrc,
-  title,
-  description,
-  buttonRef,
-}: SectionCard) {
+export default function SectionCard(props: SectionCardProps) {
   return (
     <>
       <div className=" overflow-hidden rounded-xl h-fit flex flex-col w-80 max-w-full bg-neutral drop-shadow-md border-2 shadow-neutral-dark border-neutral-dark">
         <div className="h-44 w-full relative">
           <Image
-            src={imageSrc}
-            alt={title}
+            src={props.imageSrc}
+            alt={props.title}
             layout="fill"
             objectFit="cover"
             objectPosition="center"
             className="rounded-xl"
-          />{" "}
+          />
         </div>
         <div className="flex flex-col gap-2 p-3">
-          <Title text={title} order={3} />
-          <Text text={description} showButton={false} />
-          <CustomLink href={buttonRef} linkType="button" className="w-full">
+          <Title text={props.title} order={3} />
+          <Text text={props.description} showButton={false} />
+          <CustomLink
+            href={props.buttonRef}
+            linkType="button"
+            className="w-full"
+          >
             Meer bekijken
           </CustomLink>
         </div>
