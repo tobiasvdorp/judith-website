756f89b3779603c1de2be8fea69eb9b91d99708a
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-06 14:56:45 +0200

    refactor: Remove unused imports and components from NavBar.tsx

--- src/app/components/Navbar/NavBar.tsx
+++ src/app/components/Navbar/NavBar.tsx
@@ -1,12 +1,10 @@
 "use client";
-import { Builder } from "@builder.io/react";
 import Image from "next/legacy/image";
 import Link from "next/link";
 import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import CustomLink from "../Standard/CustomLink";
-import ModeToggle from "../ThemeSwitcher";
 import { Menu, X } from "lucide-react";
 
 type NavLink = {
