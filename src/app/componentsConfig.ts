7d7e42f07c9623078770321e6bb449215fa53eb3
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-07 11:29:41 +0200

    Components added to componentsConfig

--- src/app/componentsConfig.ts
+++ src/app/componentsConfig.ts
@@ -1,21 +1,275 @@
+import { RegisteredComponent } from "@builder.io/sdk-react-nextjs";
+import Button from "@/components/Standard/Button";
+import WhatIDo from "@/components/organisms/WhatIDo";
+import { Children } from "react";
+
+// export const customComponents: RegisteredComponent[] = [
+//   {
+//     component: Button,
+//     name: "Button",
+//     inputs: [
+//       {
+//         name: "text",
+//         type: "string",
+//         required: true,
+//       },
+//     ],
+//     isRSC: true,
+//   },
+//   {
+//     component: WhatIDo,
+//     name: "WhatIDo",
+//     inputs: [
+//       {
+//         name: "title",
+//         type: "string",
+//         required: true,
+//       },
+//       {
+//         name: "description",
+//         type: "string",
+//       },
+//       {
+//         name: "sectionCards",
+//         type: "list",
+//         subFields: [
+//           {
+//             name: "imageSrc",
+//             type: "file",
+//             allowedFileTypes: ["png", "jpg", "webp", "svg", "jpeg"],
+//             required: true,
+//           },
+//           {
+//             name: "title",
+//             type: "string",
+//             required: true,
+//           },
+//           {
+//             name: "description",
+//             type: "string",
+//             required: true,
+//           },
+//           {
+//             name: "buttonRef",
+//             type: "string",
+//             required: true,
+//           },
+//         ],
+//         meta: {
+//           ts: "SectionCardProps[]",
+//         },
+//       },
+//     ],
+//   },
+// ];
 const componentsConfig = [
   {
-    name: "TestComponent",
-    path: "testComponent.tsx",
+    name: "Button",
+    path: "Standard/Button",
     inputs: [
       {
         name: "text",
         type: "string",
         required: true,
       },
+    ],
+    isRSC: true,
+  },
+
+  {
+    name: "WhatIDo",
+    path: "organisms/WhatIDo",
+    inputs: [
       {
-        name: "number",
-        friendlyName: "Numberja",
-        type: "number",
+        name: "title",
+        type: "string",
+        required: true,
+      },
+      {
+        name: "description",
+        type: "string",
+      },
+      {
+        name: "sectionCards",
+        type: "list",
+        subFields: [
+          {
+            name: "imageSrc",
+            type: "file",
+            allowedFileTypes: ["png", "jpg", "webp", "svg", "jpeg"],
+            required: true,
+          },
+          {
+            name: "title",
+            type: "string",
+            required: true,
+          },
+          {
+            name: "description",
+            type: "string",
+            required: true,
+          },
+          {
+            name: "buttonRef",
+            type: "string",
+            required: true,
+          },
+        ],
+        meta: {
+          ts: "SectionCardProps[]",
+        },
         required: true,
       },
     ],
-    isRSC: false,
+    isRSC: true,
+  },
+  {
+    name: "Banner",
+    path: "molecules/banner/Banner",
+    inputs: [
+      {
+        name: "bannerType",
+        type: "string",
+        enum: ["links", "description"],
+        required: true,
+      },
+      {
+        name: "description",
+        type: "string",
+        showIf: `options.get('bannerType') === 'description'`,
+      },
+      {
+        name: "items",
+        type: "list",
+        subFields: [
+          {
+            name: "title",
+            type: "string",
+            required: true,
+          },
+          {
+            name: "url",
+            type: "string",
+            required: true,
+          },
+        ],
+        showIf: `options.get('bannerType') === 'links'`,
+      },
+      {
+        name: "imageUrl",
+        type: "file",
+        allowedFileTypes: ["png", "jpg", "webp", "svg", "jpeg"],
+        required: true,
+      },
+      {
+        name: "alt",
+        type: "string",
+        required: true,
+      },
+      {
+        name: "title",
+        type: "string",
+        required: true,
+      },
+    ],
+    isRSC: true,
+  },
+
+  {
+    name: "WhoIAm",
+    path: "organisms/WhoIAm",
+    inputs: [
+      {
+        name: "image",
+        type: "file",
+        allowedFileTypes: ["png", "jpg", "webp", "svg", "jpeg"],
+        required: true,
+        defaultValue: "/images/placeholder.png",
+      },
+      {
+        name: "title",
+        type: "string",
+        required: true,
+        defaultValue: "Wie ben ik?",
+      },
+      {
+        name: "text",
+        type: "richText",
+        required: true,
+        defaultValue: "Text",
+      },
+      {
+        name: "buttonText",
+        type: "string",
+        required: true,
+        defaultValue: "Button",
+      },
+      {
+        name: "buttonRef",
+        type: "string",
+        required: true,
+        defaultValue: "/",
+      },
+    ],
+    isRSC: true,
+  },
+  {
+    name: "Text",
+    path: "Standard/Text",
+    inputs: [
+      {
+        name: "title",
+        type: "string",
+        friendlyName: "Title",
+      },
+      {
+        name: "text",
+        type: "richText",
+        friendlyName: "Text",
+        required: true,
+      },
+      {
+        name: "showButton",
+        type: "boolean",
+        defaultValue: false,
+        friendlyName: "Show button",
+        meta: {
+          ts: "true",
+        },
+        required: true,
+      },
+      {
+        name: "buttonText",
+        type: "string",
+        friendlyName: "Button text",
+        showIf: `options.get('showButton') === true`,
+      },
+      {
+        name: "buttonRef",
+        type: "string",
+        friendlyName: "Button ref",
+        showIf: `options.get('showButton') === true`,
+      },
+    ],
+    isRSC: true,
+  },
+  {
+    name: "MainWrapper",
+    path: "layouts/MainWrapper",
+    inputs: [
+      {
+        name: "children",
+        type: "list",
+        subFields: [
+          {
+            name: "blocks",
+            type: "uiBlocks",
+          },
+        ],
+      },
+      // andere inputs zoals nodig
+    ],
+    isRSC: true,
   },
 ];
 
@@ -23,7 +277,7 @@ export default componentsConfig;
 
 export async function loadComponents() {
   const imports = componentsConfig.map((conf) =>
-    import(`@/app/components/${conf.path}`).then((module) => ({
+    import(`@/components/${conf.path}`).then((module) => ({
       name: conf.name,
       component: module.default,
       inputs: conf.inputs,
