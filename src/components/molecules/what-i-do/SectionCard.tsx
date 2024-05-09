ee7b95ac8258082af121765001fdbd8ce2ec1e5d
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-09 12:16:13 +0200

    refactor: Update SectionCard component to support dynamic class names and update WhatIDo layout

--- src/components/molecules/what-i-do/SectionCard.tsx
+++ src/components/molecules/what-i-do/SectionCard.tsx
@@ -2,18 +2,25 @@ import Image from "next/legacy/image";
 import Title from "@/components/Standard/Title";
 import Text from "@/components/Standard/Text";
 import CustomLink from "../../Standard/CustomLink";
+import { twMerge } from "tailwind-merge";
 
 export type SectionCardProps = {
   imageSrc: string;
   title: string;
   description: string;
   buttonRef: string;
+  className?: string;
 };
 
 export default function SectionCard(props: SectionCardProps) {
   return (
     <>
-      <div className=" overflow-hidden rounded-xl h-fit flex flex-col w-80 max-w-full bg-neutral drop-shadow-md border-2 shadow-neutral-dark border-neutral-dark">
+      <div
+        className={twMerge(
+          `overflow-hidden rounded-xl h-fit w-full flex flex-col bg-neutral drop-shadow-md border-2 shadow-neutral-dark border-neutral-dark`,
+          props.className
+        )}
+      >
         <div className="h-44 w-full relative">
           <Image
             src={props.imageSrc}
@@ -26,7 +33,9 @@ export default function SectionCard(props: SectionCardProps) {
         </div>
         <div className="flex flex-col gap-2 p-3">
           <Title text={props.title} order={3} />
-          <Text text={props.description} />
+          <div className="h-fit last-card-text">
+            <Text text={props.description} />
+          </div>
           <CustomLink
             href={props.buttonRef}
             linkType="button"
--- src/components/organisms/WhatIDo.tsx
+++ src/components/organisms/WhatIDo.tsx
@@ -14,18 +14,24 @@ type WhatIDoProps = {
 export default function WhatIDo(props: WhatIDoProps) {
   return (
     <>
-      <div className="">
+      <div className="p-4">
         <SectionTitle title={props.title} />
         {props.description && (
-          <div className="w-full flex justify-center">
+          <div className="w-full text-center max-w-80 mx-auto">
             <Text text={props.description} className="text-center max-w-96" />
           </div>
         )}
-        <div className="grid lg:grid-cols-3 justify-items-center gap-5 pt-2 grid-cols-1 sm:grid-cols-2">
-          {props.sectionCards &&
-            props.sectionCards.map((sectionCard, index) => (
-              <SectionCard key={index} {...sectionCard} />
-            ))}
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-6 gap-x-3 ">
+            {props.sectionCards &&
+              props.sectionCards.map((sectionCard, index) => (
+                <SectionCard
+                  key={index}
+                  {...sectionCard}
+                  className={`${index === props.sectionCards.length - 1 && "sm:col-span-2 lg:col-span-1 last-card"}`}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </>
