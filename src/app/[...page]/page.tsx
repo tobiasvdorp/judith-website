475666af9024392551e68dc58fd35b8282386176
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-15 11:04:56 +0200

    feat: Generate metadata for homepage

--- src/app/[...page]/page.tsx
+++ src/app/[...page]/page.tsx
@@ -1,5 +1,6 @@
 import {
   Content,
+  fetchEntries,
   fetchOneEntry,
   getBuilderSearchParams,
 } from "@builder.io/sdk-react-nextjs";
@@ -7,6 +8,7 @@ import NotFound from "./not-found";
 import MainWrapper from "@/components/layouts/MainWrapper";
 import Banner from "@/components/molecules/banner/Banner";
 import { loadComponents } from "../componentsConfig";
+import { Metadata } from "next";
 
 interface MyPageProps {
   params: {
@@ -16,6 +18,35 @@ interface MyPageProps {
 }
 
 const apiKey = "87f7e6ddda884039ad862d083035a471";
+
+// Generate metadata for the homepage by fetching the homepage entry
+export async function generateMetadata(props: MyPageProps): Promise<Metadata> {
+  const { params } = props;
+  const pageUrl = "/" + params?.page?.join("/") || "";
+  console.log(pageUrl);
+  const content = await fetchOneEntry({
+    model: "page",
+    apiKey,
+    options: {
+      fields: "data.title,data.description", // Specificeer welke velden je wilt ophalen
+    },
+    userAttributes: { urlPath: pageUrl },
+  });
+
+  console.log(content);
+  return {
+    title: content?.data?.title + " - Judith van Dorp" || "Pagina",
+    description: content?.data?.description || "Pagina",
+    // image: content?.data?.bannerImage,
+    openGraph: {
+      type: "website",
+      url: "https://judithvandorp.com",
+      title: content?.data?.title + " - Judith van Dorp" || "Pagina",
+      description: content?.data?.description || "Pagina",
+      // image: content?.data?.metaImage,
+    },
+  };
+}
 
 export default async function Page(props: MyPageProps) {
   const customComponents = await loadComponents();
