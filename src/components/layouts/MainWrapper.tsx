976cb26b82bc5aab089865f9af28bfdfaf89e4af
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-15 09:54:10 +0200

    Update MainWrapper component to use py-20 for vertical padding

--- src/components/layouts/MainWrapper.tsx
+++ src/components/layouts/MainWrapper.tsx
@@ -7,7 +7,7 @@ type MainWrapperProps = {
 export default function MainWrapper(props: MainWrapperProps) {
   return (
     <>
-      <div className="w-screen flex justify-center pt-20">
+      <div className="w-screen flex justify-center py-20">
         <main
           className={twMerge(
             "w-full max-w-[1100px] flex flex-col gap-16 px-4",
