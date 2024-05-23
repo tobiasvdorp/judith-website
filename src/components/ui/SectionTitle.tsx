bb42b1e2e1de1bb1a925389d0a58357580e3185f
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-23 20:24:28 +0200

    SectionTitle line commented out

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -10,18 +10,18 @@ export default function SectionTitle({ title, className }: SectionTitle) {
     <>
       <div
         className={twMerge(
-          "w-full flex flex-row justify-center items-center",
+          "w-full flex flex-row justify-center items-center pb-4",
           className
         )}
       >
         {/* Line */}
-        <hr className="w-full text-center border border-black opacity-30" />
+        {/* <hr className="w-full text-center border border-black opacity-30" /> */}
         {/* Title */}
         <div className="w-fit text-nowrap px-3 md:px-8 text-center">
           <Title order={2} text={title} />
         </div>
         {/* Line */}
-        <hr className="w-full text-center border border-black opacity-30" />
+        {/* <hr className="w-full text-center border border-black opacity-30" /> */}
       </div>
     </>
   );
