767eaeb2630edb06d86bea2aade16bfdcbbbf30f
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-24 15:22:05 +0200

    Add key to map function

--- src/components/organisms/WhatIDo.tsx
+++ src/components/organisms/WhatIDo.tsx
@@ -29,39 +29,41 @@ export default function WhatIDo(props: WhatIDoProps) {
                 //   {...sectionCard}
                 //   className={`${index === props.sectionCards.length - 1 && ""}`}
                 // />
-                <>
-                  <div className=" bg-neutral-light w-full lg:h-64 relative rounded-xl p-6 border box-shadow-md max-w-xl">
-                    <div className="text-xl font-rodetta text-neutral-600 flex items-center gap-4">
-                      <div className="w-5">
+
+                <div
+                  className=" bg-neutral-light w-full lg:h-64 relative rounded-xl p-6 border box-shadow-md max-w-xl"
+                  key={index}
+                >
+                  <div className="text-xl font-rodetta text-neutral-600 flex items-center gap-4">
+                    <div className="w-5">
+                      {
                         {
-                          {
-                            Projecten: <FaProjectDiagram />,
-                            Coaching: <FaHandsHelping />,
-                            Buitenavonturen: <FaMountainSun />,
-                          }[sectionCard.title]
-                        }
-                      </div>
-                      {sectionCard.title}
+                          Projecten: <FaProjectDiagram />,
+                          Coaching: <FaHandsHelping />,
+                          Buitenavonturen: <FaMountainSun />,
+                        }[sectionCard.title]
+                      }
                     </div>
-                    <div className="text-neutral-500 text-sm max-w-full mb-auto">
-                      {sectionCard.description}
-                    </div>
-                    {/* <CardItem translateZ="100" className="w-full h-full mt-4">
+                    {sectionCard.title}
+                  </div>
+                  <div className="text-neutral-500 text-sm max-w-full mb-auto">
+                    {sectionCard.description}
+                  </div>
+                  {/* <CardItem translateZ="100" className="w-full h-full mt-4">
                         <Image
                           src={sectionCard.imageSrc}
                           className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                           alt="thumbnail"
                         />
                       </CardItem> */}
-                    <div className="flex lg:justify-between justify-end items-center w-full pt-2 lg:pb-0">
-                      <Button
-                        text="Meer informatie"
-                        url={sectionCard.buttonRef}
-                        className="lg:absolute lg:-bottom-5 lg:left-1/2 lg:-translate-x-1/2 w-fit text-nowrap "
-                      ></Button>
-                    </div>
+                  <div className="flex lg:justify-between justify-end items-center w-full pt-2 lg:pb-0">
+                    <Button
+                      text="Meer informatie"
+                      url={sectionCard.buttonRef}
+                      className="lg:absolute lg:-bottom-5 lg:left-1/2 lg:-translate-x-1/2 w-fit text-nowrap "
+                    ></Button>
                   </div>
-                </>
+                </div>
               ))}
           </div>
         </div>
