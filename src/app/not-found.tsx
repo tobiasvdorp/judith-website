b36c2479632b39d21b2a84f7476df655529ded18
tobiasvdorp <tobias.vandorp@student.hu.nl>
2024-05-29 19:04:24 +0200

    refactor: simplify 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,26 +3,26 @@ import Link from "next/link";
 import Image from "next/legacy/image";
 import Text from "@/components/Standard/Text";
 import Button from "@/components/Standard/Button";
+import { TbError404 } from "react-icons/tb";
 
 export default function NotFound() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className="flex gap-2 text-center flex-col items-center max-w-[28rem]">
-        <div className="relative w-full h-20 mb-4">
-          <Image
+        {/* <div className="relative w-full h-full mb-4"> */}
+        {/* <Image
             src="/images/forest.png"
             alt="Bomen"
             layout="fill"
+            objectFit="cover"
             priority
             sizes="400px"
-          />
-        </div>
-        <Title order={1} text="404 - Oeps!" />
-        <Text
-          text="Het lijkt erop dat deze pagina niet bestaat."
-          className="text-xl"
-        />
-        <Button url="/" className="mt-4" text="Naar home"></Button>
+          /> */}
+        <Title order={1} text="404" className="text-8xl" />
+        {/* </div> */}
+        {/* <Title order={1} text="Oeps!" /> */}
+        <Text text="Deze pagina bestaat niet." className="text-xl" />
+        {/* <Button url="/" className="mt-4" text="Naar home"></Button> */}
       </div>
     </div>
   );
